a21b82c582abb1b315a39ae59ec3128617ed482f
jorgeades <jorge.franco.garro@gmail.com>
2015-04-13 17:37:26 +0000

    primera subida

---
+++ EjercicioListasApi/Features/Feature1/Feature1.feature
@@ -0,0 +1,14 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f66706bd-972f-424a-880d-10fb6c25c575" description="SharePoint App Feature" featureId="f66706bd-972f-424a-880d-10fb6c25c575" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="EjercicioListasApi Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="474cd014-b3b4-4c1f-872e-b0dd07a051d2" />
+    <projectItemReference itemId="d30939cf-465a-4733-ab38-82a5e56d039a" />
+    <projectItemReference itemId="f057d63c-dce0-4340-9465-46c14fedbd6c" />
+    <projectItemReference itemId="38ecf15c-e57d-446f-8619-5a73ea20185f" />
+    <projectItemReference itemId="b0d5d46a-f299-4d7d-bf74-dba75262c3cc" />
+    <projectItemReference itemId="283101b0-1f67-4c83-979e-a579d5735469" />
+    <projectItemReference itemId="43008df7-70e1-4898-9698-fcba857408ee" />
+    <projectItemReference itemId="ae21e6ef-34f9-4d27-a8e6-51e3d67a0b54" />
+    <projectItemReference itemId="65eb195e-9c26-4813-91ac-57d90815222b" />
+  </projectItems>
+</feature>
---
+++ EjercicioListasApi/Scripts/App.js
@@ -0,0 +1,94 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+var lista;
+var datillos;
+
+function init() {
+   lista = context.get_web().get_lists().getByTitle("Datos");
+
+
+}
+
+
+function crearDatos() {
+
+    var ici = SP.ListItemCreationInformation();
+    var item = lista.addItem(ici);
+    item.set_item("Nombre", $("#txNombre").val());
+    item.set_item("Edad", $("#txEdad").val());
+
+    item.update();
+    context.load(item);
+    context.executeQueryAsync(function () {
+        alert("Datos creados con exito");
+        listadoDatos();
+    },
+        function (sender, args) {
+
+            alert(args.get_message());
+        }
+    );
+
+}
+
+
+function listadoDatos() {
+
+    datillos = lista.getItems(new SP.CamlQuery());
+    context.load(datillos);
+    context.executeQueryAsync(function () {
+        var html = "<ul>";
+
+        var enumeracion = datillos.getEnumerator();
+        while (enumeracion.moveNext()) {
+            var item = enumeracion.get_current();
+            html += "<li><a href='#' onclick='cargar(" + item.get_item("ID") + ")'>" +
+                item.get_item("Nombre") +
+                "</a> </li>";
+        }
+
+        html += "</ul>";
+
+        $("#listado").html(html);
+
+
+    }, function (sender, args) {
+        alert(args.get_message());
+    });
+
+
+}
+
+function cargar(id) {
+
+
+    var enumeracion = datillos.getEnumerator();
+    while (enumeracion.moveNext()) {
+
+        var item = enumeracion.get_current();
+        if (item.get_item("ID") == id) {
+
+            $("#Nombre").html(item.get_item("Nombre"));
+            $("#Edad").html(item.get_item("Edad"));
+            break;
+
+
+        }
+    }
+
+}
+
+
+
+$(document).ready(function () {
+
+    $("#btnAddDatos").click(function () {
+        crearDatos();
+
+    });
+    init();
+    listadoDatos();
+
+});
